574d432457f805afdd08c4d7911dc1c9713979c3
Ilya Tsygankov <ijustified@yandex.ru>
2023-02-26 11:28:11 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -166,7 +166,7 @@ setTimeout(() => {
 
 // цитата
 async function getQuotes() {  
-    const quotes = '../js/data.json';
+    const quotes = 'data.json';
     const res = await fetch(quotes);
     const data = await res.json(); 
 
@@ -190,7 +190,7 @@ async function getQuotes() {
 getQuotes();
 
 // плеер
-import playList from '../js/playList.js';
+import playList from 'playList.js';
 // console.log(playList);
 
 const audio = document.querySelector('audio');
